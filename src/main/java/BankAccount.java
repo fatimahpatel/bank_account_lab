cd9a516b4c4329ae694d68e9e3efe90f7049140f
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-15 16:24:59 +0000

    defined parameters

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,10 +1,10 @@
 public class BankAccount {
-    public BankAccount(){ //constructor
-        private String firstName;
-        private String lastName;
-        private String dateOfBirth;
-        private int accountNumber;
-        private int balance;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int accountNumber;
+    private int balance;
+    public BankAccount(String firstName, String lastName,String dateOfBirth, int accountNumber, int balance){ //constructor
 
 
     }
