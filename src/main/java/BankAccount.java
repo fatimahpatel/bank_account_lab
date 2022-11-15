29f14514bb02365c5f263750b1a047ed274cdc6e
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-15 16:21:31 +0000

    new variables for properties declared before constructor

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,5 +1,11 @@
 public class BankAccount {
     public BankAccount(){ //constructor
+        private String firstName;
+        private String lastName;
+        private String dateOfBirth;
+        private int accountNumber;
+        private int balance;
+
 
     }
 }
