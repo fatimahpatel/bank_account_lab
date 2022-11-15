f61715751213c9c3e00f740f5f54d4d145fe8f22
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-15 19:49:18 +0000

    Removed private from firstName and lastName for testing.

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,6 +1,6 @@
 public class BankAccount {
-    private String firstName;
-    private String lastName;
+    String firstName;
+    String lastName;
     private String dateOfBirth;
     private int accountNumber;
     private int balance;
