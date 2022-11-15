a8057813878eb28e71000c8fad3070ab188ebd52
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-15 19:25:44 +0000

    Assigning the arguments from Runner to declared variables.

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -5,6 +5,11 @@ public class BankAccount {
     private int accountNumber;
     private int balance;
     public BankAccount(String firstName, String lastName,String dateOfBirth, int accountNumber, int balance){ //constructor
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
 
 
     }
