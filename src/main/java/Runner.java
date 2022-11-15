d7ebb3230166604f08f69e20fa653829f05a4fcd
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-15 19:22:04 +0000

    Created two new objects, bankAccount1 and bankAccount2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,8 @@ public class Runner {
     public static void main(String[] args){ //'main' method
         //below are the arguments in ORDER
         BankAccount bankAccount = new BankAccount("Fatimah", "Patel", "21/10/1999", 12345, 0);
+        BankAccount bankAccount1 = new BankAccount("John", "Smith", "12/12/1970", 67890, 0);
+        BankAccount bankAccount2 = new BankAccount("Taylor", "Swift", "13/12/1989", 131313, 0);
 
     }
 }
